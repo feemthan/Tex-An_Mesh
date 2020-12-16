9bddc31dbaefa18786f4aa96a37a9aedc8067abc
feemthan <mohamedfaheemthanveer@gmail.com>
2020-12-17 04:26:57 +0530

    Add sample_images

--- custom_setup.sh
+++ custom_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-conda env create --file src/PIFu/environment.yml
+conda env create --file method/2_mesh_reconstruction/2_textured_mesh_with_pifu/implementation/using_pytorch/environment.yml
 conda activate PIFu
-cd method/2_mesh_recontruction/2_textured_mesh_with_pifu/implementation/using_pytorch
+cd method/2_mesh_reconstruction/2_textured_mesh_with_pifu/implementation/using_pytorch
 pip install -r requirements.txt
 sh scripts/download_trained_model.sh
+cp ../../../../../sample_images .
 sh scripts/test.sh
