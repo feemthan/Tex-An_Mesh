b57bd316a41223cc6547b176a906e11b279706b5
feemthan <mohamedfaheemthanveer@gmail.com>
2020-12-17 04:54:00 +0530

    Changes  to sample images

--- custom_setup.sh
+++ custom_setup.sh
@@ -4,5 +4,5 @@ conda activate PIFu
 cd method/2_mesh_reconstruction/2_textured_mesh_with_pifu/implementation/using_pytorch
 pip install -r requirements.txt
 sh scripts/download_trained_model.sh
-cp ../../../../../sample_images .
+cp -r  ../../../../../sample_images .
 sh scripts/test.sh
